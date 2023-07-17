2113970647b846619026dc09ecece82c9d64c0dc
Donia <doniahanyy312@gmail.com>
2023-07-17 18:23:08 +0300

    second

--- lib/main.dart
+++ lib/main.dart
@@ -91,7 +91,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // Column has various properties to control how it sizes itself and
           // how it positions its children. Here we use mainAxisAlignment to
           // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
+          // axis because Columns are vertical (the cross axis would be ddddd
           // horizontal). fgfg
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
